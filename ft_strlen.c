1402153e7ef8b18c4c6a10568c0486d9a8cfd7ce
Pascal Bartocha <pbartoch@2-A-2.42heilbronn.de>
2024-10-17 22:18:09 +0200

    create the rest of the sixth function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,14 +6,32 @@
 /*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 20:35:20 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/10/11 20:38:09 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/10/17 22:07:22 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
+// #include <stdio.h>
 
-int	ft_strlen(int c)
+int	ft_strlen(char *s)
 {
-		
+	int	length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
+
+// int	main(void)
+// {
+//     char *str = "Hallo Welt!";
+//     int length = ft_strlen(str);
+
+//     printf("Die Länge von \"%s\" ist %d\n", str, length);
+// 	return (0);
+// }
