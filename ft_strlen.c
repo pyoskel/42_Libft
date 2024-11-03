96e1f888cf97b5ef03f4efb0d6f2ebe1be5159eb
pyoskel <pasigermany@gmail.com>
2024-11-03 22:32:18 +0100

    Function optimized

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,30 +6,30 @@
 /*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 20:35:20 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/02 18:45:11 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/11/03 22:20:56 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(char *s)
+size_t	ft_strlen(const char *s)
 {
-	int	len;
+	size_t	i;
 
-	len = 0;
+	i = 0;
 	while (*s != '\0')
 	{
-		len++;
+		i++;
 		s++;
 	}
-	return (len);
+	return (i);
 }
 
 // int	main(void)
 // {
 //     char *str = "Hallo Welt!";
-//     int length = ft_strlen(str);
+//     size_t length = ft_strlen(str);
 
-//     printf("Die Länge von \"%s\" ist %d\n", str, length);
+//     printf("Die Länge von \"%s\" ist %zu\n", str, length);
 // 	return (0);
 // }
