92958d51c2ac8a644d3f00e74c36fb613d301f16
pyoskel <pasigermany@gmail.com>
2024-11-04 23:54:29 +0100

    compleded ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,34 @@
 /*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 19:58:55 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/02 20:07:27 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:53:24 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	if (d > s)
+	{
+		while (len > 0)
+		{
+			d[len] = s[len];
+			i--;
+		}
+	}
+	else
+	{
+		ft_memcpy(dest, src, len);
+	}
+	return (dest);
 }
