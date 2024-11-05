5c302046a429ee94fa7fa5b9e1646cc09f25476a
pyoskel <pasigermany@gmail.com>
2024-11-05 04:28:49 +0100

    started on ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 03:41:05 by pbartoch          #+#    #+#             */
+/*   Updated: 2024/11/05 04:28:18 by pbartoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+{
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dest_len < destsize)
+	{
+		
+	}
+	return ();
+}
