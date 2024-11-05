77e69f631dffc8b3be5de7d94930c4f29fa02e3c
pyoskel <pasigermany@gmail.com>
2024-11-05 03:09:38 +0100

    Optimized libft.h && Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 17:29:31 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/04 18:10:21 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/11/05 02:50:38 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,10 @@ int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
-void		*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t len);
 void		*ft_memset(void *b, int c, size_t len);
-const char	*ft_strchr(const char *s, int c);
+char		*ft_strchr(const char *s, int c);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlen(const char *s);
 int			ft_tolower(int c);
