62ad4c34daafecd4453e6a88e5fa2b58a92273bc
pyoskel <pasigermany@gmail.com>
2024-11-05 23:22:42 +0100

    finished ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,25 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbartoch <pbartoch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 03:41:05 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/05 04:28:18 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/11/05 23:21:51 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	dest_len;
 	size_t	src_len;
+	int		i;
 
+	i = 0;
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
-	if (dest_len < destsize)
+	if (size <= dest_len)
+		return (size + src_len);
+	while (src[i] && (dest_len + i) < (size - 1))
 	{
-		
+		dest[dest_len + i] = src[i];
+		i++;
 	}
-	return ();
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
