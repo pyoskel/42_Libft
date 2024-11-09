c52b7cb80d5fcb0151b983ed1821126054a458b0
pyoskel <pasigermany@gmail.com>
2024-11-09 21:53:16 +0100

    created ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,31 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 13:25:39 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/09 16:26:47 by pbartoch         ###   ########.fr       */
+/*   Updated: 2024/11/09 20:17:24 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	size_t	len_h;
+	size_t	len_n;
+
+	len_h = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[len_h] != '\0' && len_h < len)
+	{
+		len_n = 0;
+		while (haystack[len_h + len_n] == needle[len_n] \
+		&& needle[len_n] != '\0' && (len_h + len_n) < len)
+		{
+			len_n++;
+		}
+		if (needle[len_n] == '\0')
+			return ((char *)haystack + len_h);
+		len_h++;
+	}
+	return (NULL);
 }
