31112bb0737dd9c468e1c26b53cca6d8172e7e26
pyoskel <pasigermany@gmail.com>
2024-11-11 20:02:24 +0100

    create ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 22:02:58 by pbartoch          #+#    #+#             */
+/*   Updated: 2024/11/11 20:01:36 by pbartoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '+' && *(str + 1) != '-')
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * result);
+}
