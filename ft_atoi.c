be5c62f829f9bca5e8579b4daeb46700985fcdaa
pyoskel <pasigermany@gmail.com>
2025-01-22 22:03:57 +0100

    create a main function on atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pbartoch <pbartoch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 22:02:58 by pbartoch          #+#    #+#             */
-/*   Updated: 2024/11/11 20:01:36 by pbartoch         ###   ########.fr       */
+/*   Updated: 2025/01/22 20:09:55 by pbartoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,50 @@ int	ft_atoi(const char *str)
 	}
 	return (sign * result);
 }
+
+// int	main(void)
+// {
+//     char *test_strings[] =
+// 	{
+//         "123",
+//         "-456",
+//         "  +789",
+//         " \t\n\v\f\r  12345",
+//         "0",
+//         "-0",
+//         "+0",
+//         "2147483647", // INT_MAX
+//         "-2147483648", // INT_MIN
+//         "9999999999", // Überlauf
+//         "-9999999999", // Unterlauf
+//         "123abc",
+//         "abc123",
+//         "",
+//         NULL
+//     };
+
+//     int i = 0;
+//     while (test_strings[i] != NULL)
+//     {
+//         int original_result = atoi(test_strings[i]);
+//         int your_result = ft_atoi(test_strings[i]);
+
+//         printf("Test string: \"%s\"\n", test_strings[i]);
+//         printf("Original atoi: %d\n", original_result);
+//         printf("Deine ft_atoi: %d\n", your_result);
+
+//         if (original_result == your_result)
+//         {
+//             printf("--> OK\n");
+//         }
+//         else
+//         {
+//             printf("--> FEHLER!\n");
+//         }
+
+//         printf("\n");
+//         i++;
+//     }
+
+//     return EXIT_SUCCESS;
+// }
